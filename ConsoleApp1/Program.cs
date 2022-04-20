0f76c26b8a323a0b831e47a2914b81ca37707e28
Kirill Bulyshkin <k.bulyshkin@a1qa.com>
2022-04-20 15:22:55 +0400

    Implementing  sending requests using cycle

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
-
-using System;
+﻿using System;
 using System.Threading.Tasks;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -12,36 +9,30 @@ namespace WebAPIClient
     class Program
     {
         private static readonly HttpClient client = new HttpClient();
-        //private static async Task ProcessRepositories()
-        //{
-        //    //client.defaultrequestheaders.accept.clear();
-        //    //client.defaultrequestheaders.accept.add(
-        //    //new mediatypewithqualityheadervalue("application/vnd.github.v3+json"));
-        //    //client.defaultrequestheaders.add("user-agent", ".net foundation repository reporter");
 
-        //    var stringTask = client.GetStringAsync("https://dev.by");
-
-        //    var msg = await stringTask;
-        //    Console.Write(msg);
-        //}
+        private static string website = "https://dev.by";
 
         static async Task Main(string[] args)
         {
-            //await ProcessRepositories();
             var anInstanceofProgramClass = new Program();
-            await anInstanceofProgramClass.showAsyncTime("https://dev.by");
-            //await showAsyncTime("https://dev.by");
+            await anInstanceofProgramClass.showAsyncTime(website);
         }
 
         public async Task showAsyncTime(string url)
         {
-            var sw = new Stopwatch();
-            sw.Restart();
-            await client.GetStringAsync(url);
-            sw.Stop();
+            while (true)
+            {   
+                var sw = new Stopwatch();
+                sw.Restart();
+                await client.GetStringAsync(url);
+                sw.Stop();
 
-            Console.WriteLine(sw.ElapsedMilliseconds);
+                Console.WriteLine($"Response time of the requested website '{website}' is {sw.ElapsedMilliseconds}");
+
+                Thread.Sleep(1000);
+            }
         }
 
     }
+
 }
